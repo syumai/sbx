03f3e2382afc204be2dc17ad1035ea31bff5c82e
syumai <syumai@gmail.com>
2025-04-18 02:00:55 +0900

    add urfave

--- cmd/sbx/main.go
+++ cmd/sbx/main.go
@@ -1,3 +1,28 @@
 package main
 
-func main() {}
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/urfave/cli/v3"
+)
+
+func main() {
+	cmd := &cli.Command{
+		Name: "sbx",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:  "allow-file",
+				Usage: "path to the allow file",
+			},
+			&cli.StringFlag{
+				Name:  "deny-file",
+				Usage: "path to the deny file",
+			},
+		},
+	}
+	if err := cmd.Run(context.Background(), os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/syumai/sbx
 
 go 1.24.1
+
+require github.com/urfave/cli/v3 v3.1.1
